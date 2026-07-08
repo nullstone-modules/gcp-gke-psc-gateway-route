a5d4c4ca8c7bd8abce6626e6a21132d78cac5f0a
Brad Sickles <brad.sickles@gmail.com>
2026-07-07 21:41:28 -0400

    Configure hostname on route to <app-name>.<service_domain>

--- gateway.tf
+++ gateway.tf
@@ -6,4 +6,10 @@ data "ns_connection" "gateway" {
 locals {
   gateway_name      = data.ns_connection.gateway.outputs.gateway_name
   gateway_namespace = data.ns_connection.gateway.outputs.gateway_namespace
+  service_domain    = data.ns_connection.gateway.outputs.service_domain
+
+  // Apps on the gateway are addressed as <app-name>.<service_domain> (i.e.
+  // "<app-name>.<dns_name>.<internal_domain>"). The load balancer routes by Host header,
+  // so consumers must resolve the gateway's service domain to their PSC endpoint.
+  hostname = "${local.block_name}.${local.service_domain}"
 }
